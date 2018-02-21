6221115a29e9323c034436f799b1853c9bd991d6
Geetam Chawla <geetam.chawla@gmail.com>
2018-02-21 21:07:41 +0530

    initialize num_errors using a constructor

--- assignments/PA4/semanterror.cc
+++ assignments/PA4/semanterror.cc
@@ -4,6 +4,9 @@ extern char *curr_filename;
 void SemantError::print_error(int lineno, const std::string &error_msg)
 {
     num_errors++;
-    std::cerr << curr_filename << ": " << lineno << " error: " << error_msg;
+    std::cerr << curr_filename << ":" << lineno << " error: " << error_msg << "\n";
 }
 
+SemantError::SemantError() : num_errors(0)
+{
+}
--- assignments/PA4/semanterror.h
+++ assignments/PA4/semanterror.h
@@ -5,11 +5,12 @@
 
 class SemantError {
 private:
-  int num_errors;
+    int num_errors;
 
 public:
-  int get_num_errors() { return num_errors; }
-  void print_error(int lineno, const std::string &error_msg);
+    SemantError();
+    int get_num_errors() { return num_errors; }
+    void print_error(int lineno, const std::string &error_msg);
 };
 
 
