7f0e587ccdf3873482430282b785b75b0c3de9b8
Geetam Chawla <geetam.chawla@gmail.com>
2018-02-22 00:07:36 +0530

    color error red

--- assignments/PA4/semanterror.cc
+++ assignments/PA4/semanterror.cc
@@ -4,7 +4,7 @@ extern char *curr_filename;
 void SemantError::print_error(int lineno, const std::string &error_msg)
 {
     num_errors++;
-    std::cerr << curr_filename << ":" << lineno << " error: " << error_msg << "\n";
+    std::cerr << curr_filename << ":" << lineno << " \033[31merror\033[0m: " << error_msg << "\n";
 }
 
 SemantError::SemantError() : num_errors(0)
