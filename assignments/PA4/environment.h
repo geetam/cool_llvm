d8f0dfe9c13618a4a56d74db8457b14bb802f322
Geetam Chawla <geetam.chawla@gmail.com>
2018-03-11 00:27:38 +0530

    make compile

--- assignments/PA4/environment.h
+++ assignments/PA4/environment.h
@@ -6,12 +6,19 @@
 #include "stringtab.h"
 
 class Environment {
+private:
+    std::map < std::pair <Symbol, Symbol>, type_vec> method_env;
+    bool built_method_env;
 public:
+    Environment();
+    std::map < std::pair <Symbol, Symbol>, type_vec> get_method_env();
+    
     InheritanceGraph igraph;
     symtab_sym_dat symbol_table;
     std::map <Symbol, class_atts> classname_atts_map;
     std::map <Symbol, class_methods> classname_methods_map;
     Symbol current_class;
+    
 };
 
 #endif
